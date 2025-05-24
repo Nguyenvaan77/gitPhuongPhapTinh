cd7e505db1f43a50e645b0711268a2639b42d74f
emsidt <trinhbinhduong200x@gmail.com>
2025-05-24 23:08:58 +0700

    update_get_random_data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from data import get_sample_data
+from data import get_sample_data, get_random_data
 from plot import plot_spline
 from spline import natural_cubic_spline
 # from spline_gauss import natural_cubic_spline_gauss
 
 def main():
-    x, y = get_sample_data()
+    x, y = get_random_data()
 
     a, b, c, d = natural_cubic_spline(x, y)
     # a, b, c, d = spline_gauss(x, y)
