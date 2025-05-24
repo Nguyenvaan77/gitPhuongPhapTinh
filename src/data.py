5f6724c069ce4d865e32ffcc0662bc371de273d0
emsidt <trinhbinhduong200x@gmail.com>
2025-05-24 22:38:07 +0700

    1.0

---
+++ src/data.py
@@ -0,0 +1,6 @@
+def get_sample_data():
+
+    x = [0, 1, 2, 3, 4]
+    y = [0, 1, 0, 1, 0]
+
+    return x, y
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from data import get_sample_data
+from plot import plot_spline
+from spline import natural_cubic_spline
+# from spline_gauss import natural_cubic_spline_gauss
+
+def main():
+    x, y = get_sample_data()
+
+    a, b, c, d = natural_cubic_spline(x, y)
+    # a, b, c, d = spline_gauss(x, y)
+
+    plot_spline(x, y, a, b, c, d)
+
+if __name__ == '__main__':
+    main()
